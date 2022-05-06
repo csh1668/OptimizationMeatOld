4e07af1ac8144c9006a690b3c14a4f94a294c63f
mlholland <mlholland@users.noreply.github.com>
2022-05-06 11:09:24 -0400

    Add Compat file
    Doesn't include compile changes because I had to severely mess up the project settings to make this build locally.

---
+++ Compatibility/Compatibility_MoolohsDndMenagerie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/** Certain animals from Mooloh's DND Menagerie drop non-meat items (namely steel and sandstone). We need to whitelist these not-really-meat-dropping animals
+ * so this mod doesn't think that steel and stone are meat and bork the game trying to reconcile that idea.
+ * */
+namespace AlienMeatTest.Compatibility
+{
+    public class Compatibility_MoolohsDndMenagerie : Compatibility
+    {
+        protected override string PackageID => "mooloh.dndmenagerie";
+        public override bool IsPreOptimization => true;
+        public override void DoPatch()
+        {
+            if (!DetectMod()) return;
+            MeatLogger.Debug("Mooloh's DND Menagerie!");
+
+            MeatOptimization.WhiteListRace.Add("DND_GolemSteel");
+            MeatOptimization.WhiteListRace.Add("DND_GolemStone");
+            MeatOptimization.WhiteListRace.Add("DND_AnimatedToolCabinet"); 
+            MeatOptimization.WhiteListRace.Add("DND_AnimatedDiningChair");
+            MeatOptimization.WhiteListRace.Add("DND_AnimatedStool");
+        }
+    }
+}
